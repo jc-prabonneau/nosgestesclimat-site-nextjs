205b6dda6bc1537d78c0f3c8f7afbd655d2fc3a2
Florian <hi@florianpanchout.com>
2024-02-27 10:02:18 +0100

    fix: use correct api path

--- src/helpers/modelFetching/getFileFromModel.ts
+++ src/helpers/modelFetching/getFileFromModel.ts
@@ -45,7 +45,7 @@ async function importPreviewFile({
 async function importFile({ fileName }: { fileName: string }) {
   console.log('fetching file', fileName)
   return axios
-    .get(`/api/${fileName}`)
+    .get(`/api/model/${fileName}`)
     .then((res) => res.data)
     .catch((e) => {
       console.error('importFile error', e)
